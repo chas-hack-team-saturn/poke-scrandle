d0d254121d4da1051440021fefdf8fd09ea630b1
Alex <alexcoh19@outlook.com>
2026-01-30 16:59:05 +0100

    API WORKS WOOOOOO

--- BackendAPI/Data/PokeScrandleDbContext.cs
+++ BackendAPI/Data/PokeScrandleDbContext.cs
@@ -1,15 +1,52 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using System;
+using System.Collections.Generic;
 using BackendAPI.Models;
+using Microsoft.EntityFrameworkCore;
+using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
 
-namespace BackendAPI.Data
+namespace BackendAPI.Data;
+
+public partial class PokeScrandleDbContext : DbContext
 {
-    public class PokeScrandleDbContext : DbContext
+    public PokeScrandleDbContext()
     {
-        public PokeScrandleDbContext(DbContextOptions<PokeScrandleDbContext> options) : base(options)
-        {
-
-        }
-
-        public virtual DbSet<Pokemon> Pokemon { get; set; }
     }
+
+    public PokeScrandleDbContext(DbContextOptions<PokeScrandleDbContext> options)
+        : base(options)
+    {
+    }
+
+    public virtual DbSet<Pokemon> Pokemons { get; set; }
+
+//    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder);
+
+    protected override void OnModelCreating(ModelBuilder modelBuilder)
+    {
+        modelBuilder
+            .UseCollation("utf8mb4_uca1400_ai_ci")
+            .HasCharSet("utf8mb4");
+
+        modelBuilder.Entity<Pokemon>(entity =>
+        {
+            entity.HasKey(e => e.DexId).HasName("PRIMARY");
+
+            entity.ToTable("Pokemon");
+
+            entity.HasIndex(e => e.Name, "UX_Pokemon_Name").IsUnique();
+
+            entity.Property(e => e.DexId).HasColumnType("int(11)");
+            entity.Property(e => e.Name)
+                .HasMaxLength(50)
+                .UseCollation("utf8mb3_uca1400_ai_ci")
+                .HasCharSet("utf8mb3");
+            entity.Property(e => e.Votes)
+                .HasDefaultValueSql("'0'")
+                .HasColumnType("int(11)");
+        });
+
+        OnModelCreatingPartial(modelBuilder);
+    }
+
+    partial void OnModelCreatingPartial(ModelBuilder modelBuilder);
 }
